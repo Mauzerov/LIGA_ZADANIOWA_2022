a5ebc17825b0bfbfc290f98d6c227798549ad12c
Mauzerov <mauzer.official.1@gmail.com>
2022-12-09 19:02:54 +0100

    Zadanie4

--- 4/Zadanie4.py
+++ 4/Zadanie4.py
@@ -20,17 +20,14 @@ def try_input(
 
 class Player:
     score: int
-    """ `0` means no multiplier """
     strike_multiplier: [int]
-    """ `0` means no multiplier """
     spare_multiplier: int
-    throws: [int]
+    number: int
 
     def __init__(self):
         self.score = 0
         self.strike_multiplier = []
         self.spare_multiplier = 0
-        self.throws = []
 
     def get_multiplier_and_reduce(self):
         for i in range(len(self.strike_multiplier)):
@@ -41,17 +38,16 @@ class Player:
 
     def require_multiplier(self):
         flag = self.strike_multiplier or self.spare_multiplier
-        #self.strike_multiplier -= max(0, self.strike_multiplier - 1)
         self.spare_multiplier = max(self.spare_multiplier - 1, 0)
         return not not flag
 
     def set_strike_multiplier(self):
         self.strike_multiplier += [2]
-        print("STRIKE")
+        print("Strike")
 
     def set_spare_multiplier(self):
         self.spare_multiplier += 1
-        print("SPARE")
+        print("Spare")
 
 
 class Throw:
@@ -59,16 +55,15 @@ class Throw:
         self.pins = 10
 
     def __call__(self, pins: int) -> bool:
-        if pins == 0:
-            print("Miss")
         self.pins -= pins
         return self.pins == 0
 
 
 class Frame:
+    COUNT = 10
     number: int
 
-    def __init__(self, number: int, players: [Player]):
+    def __init__(self, number: int, players: [Player], on_frame_end: typing.Callable = lambda: None):
         self.number = number
         print(f"Frame {number}")
         for player in players:
@@ -76,10 +71,9 @@ class Frame:
 
             pins = try_input(f"Player {player.number} shoot: ",
                              check=lambda x: 0 <= int(x) <= 10, convert=int,
-                             error="Proszę wprowadzić liczbę całkowitą w zakresie od 0 do 10")
+                             error="Wprowadź ponownie")
 
             throw = Throw()
-            player.throws.append(0)
             if throw(pins):
                 player.set_strike_multiplier()
             else:
@@ -89,30 +83,47 @@ class Frame:
                 multiplier = player.get_multiplier_and_reduce()
                 pins = try_input(f"Player {player.number} shoot: ",
                                  check=lambda x: 0 <= int(x) <= throw.pins, convert=int,
-                                 error=f"Proszę wprowadzić liczbę całkowitą w zakresie od 0 do {throw.pins}")
+                                 error="Wprowadź ponownie")
                 if throw(pins):
                     player.set_spare_multiplier()
+
             player.score += pins + (pins * multiplier)
-            player.throws[-1] += pins + (pins * multiplier)
-        # Todo Last frame (10th) is different
+
+            if throw.pins == 10:
+                print("Miss")
+
+            if number == Frame.COUNT:
+                if throw.pins == 0:
+                    multiplier = player.get_multiplier_and_reduce()
+                    pins = try_input(f"Player {player.number} shoot: ",
+                                     check=lambda x: 0 <= int(x) <= 10, convert=int,
+                                     error="Wprowadź ponownie")
+                    if pins == 10:
+                        player.set_strike_multiplier()
+                    player.score += pins + (pins * multiplier)
+        on_frame_end()
+
 
 class Game:
     def __init__(self, players: ['Player']):
         self.players = players
-        self.frames = [Frame(i, players) for i in range(1, 11)]
+        self.frames = [Frame(i, players, self.scoreboard) for i in range(1, Frame.COUNT + 1)]
 
     def scoreboard(self):
-        print("-------" + "-" * len(str(len(self.players))))
+        print("-------" + "-" * len(str(len(self.players))) + "+-" + \
+              "-" * len(str(max(self.players, key=lambda x: x.score).score)))
         for i, player in enumerate(self.players):
             print(f"Gracz {i + 1} | {player.score}")
+
 
 def main():
     players = [Player() for _ in range(try_input("Ile graczy? ", check=lambda x: 0 < int(x) <= 4, convert=int))]
     for i, player in enumerate(players):
         player.number = i + 1
     game = Game(players)
-    for player in players:
-        print(f"Player {player.number} score: {player.score} {player.throws}")
+    #game.scoreboard()
+
+    print("Wygrał gracz numer", max(players, key=lambda x: x.score).number)
 
 
 if __name__ == '__main__':
