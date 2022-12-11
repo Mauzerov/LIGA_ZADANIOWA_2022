9c94d9ce4b9a80d54b532aa7aaf7e63e2aa9e2b1
Mauzerov <mauzer.official.1@gmail.com>
2022-12-11 19:14:34 +0100

    zadanie5 gallery

--- 5/script.js
+++ 5/script.js
@@ -11,3 +11,17 @@ document.querySelectorAll('.s7d-digit').forEach((el) => {
         el.append(segment)
     }
 })
+
+document.querySelectorAll('.carousel__control').forEach((el) => {
+    el.addEventListener('click', (e) => {
+        const carousel = e.target.closest('.carousel')
+        const items = carousel.querySelectorAll('.carousel__item')
+        const activeItem = carousel.querySelector('.carousel__item.active')
+        const activeItemIndex = Array.from(items).indexOf(activeItem)
+        const direction = parseInt(el.dataset.value)
+        const itemToActivate = items[(activeItemIndex + direction + items.length) % items.length]
+        carousel.querySelector('.carousel__container').scrollLeft += itemToActivate.offsetWidth * direction
+        activeItem.classList.remove('active')
+        itemToActivate.classList.add('active')
+    })
+})
